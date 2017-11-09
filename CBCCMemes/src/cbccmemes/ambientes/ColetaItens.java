530ee2db409351d427ee692f03d5977057658623
Kelly Costa <kellydosocorro@gmail.com>
2017-11-08 23:53:03 -0200

    Update ColetaItens.java

--- CBCCMemes/src/cbccmemes/ambientes/ColetaItens.java
+++ CBCCMemes/src/cbccmemes/ambientes/ColetaItens.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cbccmemes.ambientes;
 import cbccmemes.personagens.Personagem;
 import cbccmemes.mochila.Item;
 
-/**
- *
- * @author 201604940039
- */
 public class ColetaItens extends Ambiente{
     private Item item;
     //Definir array de Itens abaixo
