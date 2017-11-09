924ff86cae719b57a3ba4b15d82da18f66203a66
Kelly Costa <kellydosocorro@gmail.com>
2017-11-08 23:55:53 -0200

    Update Aluno.java

--- CBCCMemes/src/cbccmemes/personagens/Aluno.java
+++ CBCCMemes/src/cbccmemes/personagens/Aluno.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cbccmemes.personagens;
 
 import cbccmemes.TelaPrincipal;
 import cbccmemes.ambientes.Ambiente;
 import cbccmemes.mochila.*;
 
-/**
- *
- * @author 201604940039
- */
 public class Aluno extends Personagem{
     private static final int NIVEL_MAXIMO = 8;
     private static final String nomes_alunos[] = {"Lucas","Ian","José","Jairo","Kelly"};
