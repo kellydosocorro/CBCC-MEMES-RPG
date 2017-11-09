013ef72e2e560aa0248db01883df82a83198512e
Kelly Costa <kellydosocorro@gmail.com>
2017-11-08 23:52:06 -0200

    Update Ambiente.java

--- CBCCMemes/src/cbccmemes/ambientes/Ambiente.java
+++ CBCCMemes/src/cbccmemes/ambientes/Ambiente.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cbccmemes.ambientes;
 
 import cbccmemes.personagens.Personagem;
 
-/**
- *
- * @author 201604940039
- */
 public abstract class Ambiente {
     private final String nome;
     private int nivel_minimo;
