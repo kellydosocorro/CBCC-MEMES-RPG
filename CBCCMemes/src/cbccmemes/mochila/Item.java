a760b13e12118069de432c538c489f3c2576839b
Kelly Costa <kellydosocorro@gmail.com>
2017-11-08 23:55:15 -0200

    Update Item.java

--- CBCCMemes/src/cbccmemes/mochila/Item.java
+++ CBCCMemes/src/cbccmemes/mochila/Item.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cbccmemes.mochila;
 
 import cbccmemes.personagens.Personagem;
 import java.util.Random;
 
-/**
- *
- * @author 201604940039
- */
 public class Item {
     private int nivel_minimo;
     private String nome;
